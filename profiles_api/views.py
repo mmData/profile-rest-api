c0c35571099053ae784bba8fb2733c0904b3be08
“9db89ced737607f54e1e8c265693e25e863c125f” <marco.mancini@sap.com>
2020-07-07 08:53:10 +0200

    h

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,14 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from profiles_api import models
+from profiles_api import permissions
+from rest_framework.permissions import IsAuthenticated
+
 
 class HelloApiView(APIView):
 	"""Test API View"""
@@ -89,7 +97,29 @@ class HelloViewSet(viewsets.ViewSet):
 		"""handle removing an object """
 		return Response({'http_method':'DELETE'})
 
+class UserProfileViewSet(viewsets.ModelViewSet):
+	"""Handle creating and updating profiles"""
+	serializer_class = serializers.UserProfileSerializer
+	queryset = models.UserProfile.objects.all()
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (permissions.UpdateOwnProfile,)
+	filter_backends = (filters.SearchFilter,)
+	search_fields = ('name','email',)
 
+class UserLoginApiView(ObtainAuthToken):
+	"""Handle creating user authentication tokens"""
+	renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+	"""Handles creating, reading and updating profile feed items"""
+	authentication_classes = (TokenAuthentication,)
+	serializer_class = serializers.ProfileFeedItemSerializer
+	queryset = models.ProfileFeedItem.objects.all()
+	permission_classes = (permissions.UpdateOwnStatus,IsAuthenticated)
+
+	def perform_create(self, serializer):
+		"""Sets the user profile to the logges in user """
+		serializer.save(user_profile=self.request.user)
 
 
 
